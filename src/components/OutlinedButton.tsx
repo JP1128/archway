f4581f79763fbc2bd4e99b170823e87153f2a776
Grace <ger85847@uga.edu>
2025-04-10 19:18:08 -0400

    buttons 2

--- src/components/OutlinedButton.tsx
+++ src/components/OutlinedButton.tsx
@@ -1,15 +1,17 @@
-const OutlinedButton = (props) => {
-    return (
-      <button 
-        className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-[15px] py-[7px] rounded-[10px] border border-[#fa5053]"
-        type={props.type || 'button'}
-        onClick={props.onClick}
-      >
-        <p className="flex-grow-0 flex-shrink-0 text-xs font-semibold text-left text-[#fa5053]">
-            {props.children}
-        </p>
-      </button>
-    );
+import React from 'react';
+
+const OutlinedButton: React.FC<{ type?: 'button' | 'submit' | 'reset'; onClick?: React.MouseEventHandler<HTMLButtonElement>; children: React.ReactNode }> = ({ type = 'button', onClick, children }) => {
+  return (
+    <button
+      className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-[15px] py-[7px] rounded-[10px] border border-[#fa5053]"
+      type={type}
+      onClick={onClick}
+    >
+      <p className="flex-grow-0 flex-shrink-0 text-xs font-semibold text-left text-[#fa5053]">
+        {children}
+      </p>
+    </button>
+  );
 };
-  
+
 export default OutlinedButton;
