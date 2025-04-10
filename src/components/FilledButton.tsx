3e113b34b0d87a84dbdb5900598e6b2974fcc898
Grace <ger85847@uga.edu>
2025-04-10 19:18:08 -0400

    buttons 2

--- src/components/FilledButton.tsx
+++ src/components/FilledButton.tsx
@@ -1,15 +1,13 @@
-const FilledButton = (props) => {
-    return (
-      <button 
-        className="flex flex-col justify-start items-start relative overflow-hidden gap-2.5 px-[15px] py-[7px] rounded-[10px] bg-[#fa5053]"
-        type={props.type || 'button'}
-        onClick={props.onClick}
-      >
-        <p className="flex-grow-0 flex-shrink-0 text-xs font-semibold text-left text-white">
-            {props.children}
-        </p>
-      </button>
-    );
+import React from 'react';
+
+const FilledButton: React.FC<{ label: string }> = ({ label }) => {
+  return (
+    <button
+      className="flex flex-col justify-start items-start relative overflow-hidden gap-2.5 px-[15px] py-[7px] rounded-[10px] bg-[#fa5053]"
+    >
+      {label}
+    </button>
+  );
 };
-  
+
 export default FilledButton;
--- src/components/TextButton.tsx
+++ src/components/TextButton.tsx
@@ -1,14 +1,15 @@
-const TextButton = (props) => {
-    return (
-      <button 
-        className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-[15px] py-[7px] rounded-[10px] border border-[#fa5053]"
-        type={props.type || 'button'}
-        onClick={props.onClick}
-      >
-        
-        {props.children}
-      </button>
-    );
+import React from 'react';
+
+const TextButton: React.FC<{ type?: 'button' | 'submit' | 'reset'; onClick?: React.MouseEventHandler<HTMLButtonElement>; children: React.ReactNode }> = ({ type = 'button', onClick, children }) => {
+  return (
+    <button
+      className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative overflow-hidden gap-2.5 px-[15px] py-[7px] rounded-[10px] bg-transparent text-black border-none"
+      type={type}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
-  
+
 export default TextButton;
