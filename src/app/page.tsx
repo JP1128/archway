a2ce9305432a84b83165fa2631ead95adab7f64d
Grace <ger85847@uga.edu>
2025-04-14 18:49:50 -0400

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import MiddleSplashCards from "@/components/MiddleSplashCards";
 import Image from "next/image";
 
 export default function Home() {
     return (
-    <>
-    <MiddleSplashCards></MiddleSplashCards>
-    </>
-    );
-    /* return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
                 <Image
@@ -105,5 +99,5 @@ export default function Home() {
                 </a>
             </footer>
         </div>
-    ); */
+    );
 }
